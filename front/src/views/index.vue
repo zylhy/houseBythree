b5abc0e59965ca491958605954debc750dbf80bd
zylhy <1097267712@qq.com>
2023-11-25 16:09:53 +0800

    房顶搭建，相机改为透视相机，使用第一人称控制器

--- front/src/views/index.vue
+++ front/src/views/index.vue
@@ -31,16 +31,18 @@ const initThree = () => {
   scene.add(axes);
   render.setSize(canvas.width, canvas.height);
   // 新建一个 相机
-  const camera = new THREE.PerspectiveCamera(60, canvas.width / canvas.height, 0.1, 1000);
-  camera.position.set(50, 50, 50);
-  camera.lookAt(scene.position);
+ 
+const camera = new THREE.PerspectiveCamera(60, canvas.width / canvas.height, 0.1, 1000);
+  camera.position.set(0,30,50);
+  camera.lookAt(0,30,0);
   // 创建一个第一人称控制器
-  // const controls = new FirstPersonControls(camera, canvas);
-  // controls.movementSpeed = 10;
-  // controls.lookVertical = false;
+  const controls = new FirstPersonControls(camera, canvas);
+    controls.lookSpeed = 0.05;
+  controls.movementSpeed =50;
+  controls.lookVertical = false;
   const clock = new THREE.Clock();
   // 创建轨道控制器
-  const textControls = new OrbitControls(camera, canvas);
+  // const textControls = new OrbitControls(camera, canvas);
   // 创建一个环境光
   const light = new THREE.AmbientLight(0xcccccc);
   scene.add(light);
@@ -49,9 +51,9 @@ const initThree = () => {
   // 渲染动画
   function animate() {
     render.render(scene, camera);
-    textControls.update();
+    // textControls.update();
     const delta = clock.getDelta();
-    // controls.update(delta);
+    controls.update(delta);
     requestAnimationFrame(animate);
   }
   animate();
@@ -74,19 +76,37 @@ const createGround = () => {
   ground.receiveShadow = true; // 接收阴影
   gameScene.value.add(ground);
 };
+
 // 新建房子
 const createHouse = () => {
   createFloor();
   createWall();
-  let  roof1 = createRoof()
-  let  roof2 = createRoof()
-  // roof1.rotation.y = roof2.rotation.y = 0.5*Math.PI
-// roof2.rotation.y = Math.PI / 4 * 0.6;
-  // house.add(roof1)
-roof2.rotation.x = Math.PI / 4 ;
-// roof2.rotation.y = Math.PI / 2  ;
-console.log(roof2)
-  house.add(roof2)
+  let roof1 = createRoof();
+  let roof2 = createRoof();
+
+  //模型对象旋转的函数，每次设置一个坐标轴的变换
+  function rotateAroundWorldAxis(object, axis, radians) {
+    let rotWorldMatrix = new THREE.Matrix4();
+    rotWorldMatrix.makeRotationAxis(axis.normalize(), radians);
+    rotWorldMatrix.multiply(object.matrix);
+    object.matrix = rotWorldMatrix;
+    object.rotation.setFromRotationMatrix(object.matrix);
+  }
+
+  //调用方式，设置x、y、z轴的旋转
+  let xAxis = new THREE.Vector3(1, 0, 0);
+  let yAxis = new THREE.Vector3(0, 1, 0);
+  let zAxis = new THREE.Vector3(0, 0, 1);
+  //模型、旋转轴和旋转角度（弧度）
+  rotateAroundWorldAxis(roof1, yAxis, Math.PI / 2);
+  rotateAroundWorldAxis(roof2, yAxis, Math.PI / 2);
+  rotateAroundWorldAxis(roof1, zAxis, -Math.PI / 2.8);
+  rotateAroundWorldAxis(roof2, zAxis, -Math.PI /1.55);
+  roof1.position.y = roof2.position.y = 120;
+  roof1.position.x = -62;
+  roof2.position.x = 62;
+  house.add(roof2);
+  house.add(roof1);
   house.add(createWall(-100));
   house.add(createWall(100));
   house.add(createBehindWall());
@@ -144,57 +164,72 @@ const createBehindWall = () => {
 
   let material = new THREE.MeshBasicMaterial({ map: texture });
   let sidewall = new THREE.Mesh(extrudeGeometry, material);
-  sidewall.rotation.y = 0.5*Math.PI
-  sidewall.position.x = -100
+  sidewall.rotation.y = 0.5 * Math.PI;
+  sidewall.position.x = -100;
   return sidewall;
 };
-const createFrontWall = () =>{
-// 定义一个形状
-const shape = new THREE.Shape();
-shape.moveTo(-100,0)
-shape.lineTo(100, 0);
+const createFrontWall = () => {
+  // 定义一个形状
+  const shape = new THREE.Shape();
+  shape.moveTo(-100, 0);
+  shape.lineTo(100, 0);
   shape.lineTo(100, 100);
   shape.lineTo(-100, 100);
   shape.lineTo(-100, 0);
 
   // 挖一个窗户
-  const window = new THREE.Path()
-  window.moveTo(30,30)
-  window.lineTo(30,50)
-  window.lineTo(80,50)
-  window.lineTo(80,30)
-  window.lineTo(30,30)
-  shape.holes.push(window)
+  const window = new THREE.Path();
+  window.moveTo(30, 30);
+  window.lineTo(30, 50);
+  window.lineTo(80, 50);
+  window.lineTo(80, 30);
+  window.lineTo(30, 30);
+  shape.holes.push(window);
 
   // 挖一个们
-const door  = new  THREE.Path()
-door.moveTo(-30,0)
-door.lineTo(-30,80)
-door.lineTo(-80,80)
-door.lineTo(-80,0)
-door.lineTo(-30,0)
-shape.holes.push(door)
-  const extrudeGeometry = new THREE.ExtrudeGeometry(shape,{depth:5})
+  const door = new THREE.Path();
+  door.moveTo(-30, 0);
+  door.lineTo(-30, 80);
+  door.lineTo(-80, 80);
+  door.lineTo(-80, 0);
+  door.lineTo(-30, 0);
+  shape.holes.push(door);
+  const extrudeGeometry = new THREE.ExtrudeGeometry(shape, { depth: 5 });
   // 定义纹理
-  const texture = new THREE.TextureLoader().load(requireAssets('wall.jpg'))
-  texture.wrapS = texture.wrapT =  THREE.RepeatWrapping;
-  texture.repeat.set(0.01,0.01)
-  const material = new THREE.MeshBasicMaterial({map:texture})
+  const texture = new THREE.TextureLoader().load(requireAssets("wall.jpg"));
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set(0.01, 0.01);
+  const material = new THREE.MeshBasicMaterial({ map: texture });
 
-  const frontWall =  new THREE.Mesh(extrudeGeometry,material)
-  frontWall.rotation.y = 0.5*Math.PI
-  frontWall.position.x = 95
-  return frontWall
-}
+  const frontWall = new THREE.Mesh(extrudeGeometry, material);
+  frontWall.rotation.y = 0.5 * Math.PI;
+  frontWall.position.x = 95;
+  return frontWall;
+};
 // 创建屋顶
-const createRoof = ()=>{
+const createRoof = () => {
   // 定义几何体
-  const geometry  = new THREE.BoxGeometry(200,100,5) 
+  const geometry = new THREE.BoxGeometry(210, 140, 5);
+  // 定义一个纹理
+  const texture = new THREE.TextureLoader().load(requireAssets("tile.jpg"));
+  texture.repeat.set(5, 5);
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   // 定义材质
-  const material = new THREE.MeshBasicMaterial({color: 'grey'});
-let roof = new THREE.Mesh(geometry,material)
-return roof
-}
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  // 定义颜色材质
+  const colorMaterial = new THREE.MeshBasicMaterial({ color: "gray" });
+  const materials = [
+    colorMaterial,
+    colorMaterial,
+    colorMaterial,
+    colorMaterial,
+    colorMaterial,
+    material,
+  ];
+  let roof = new THREE.Mesh(geometry, materials);
+
+  return roof;
+};
 onMounted(() => {
   initThree();
 });
